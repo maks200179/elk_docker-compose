57c748af2f447cd846f80e92319ac1c790a5bdd9
root <root@docker.dockhub.com>
2019-05-21 17:12:14 -0400

    added docker compose elk

---
+++ clean_all_dockers_env.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
+
+docker volume prune -f
+docker system prune -a  -f 
+ 
+
+docker rmi $(docker images -q)
+
+
+systemctl stop docker
+rm -rf /var/lib/docker/overlay2/*
+systemctl start  docker 
+
+
